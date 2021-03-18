6b56053912a4f8907e9e9d66ae1164398b639823
dizurix <malo7414@gmail.com>
2021-03-18 10:11:56 +0100

    j'aime bien

--- src/Controller/GedController.php
+++ src/Controller/GedController.php
@@ -9,6 +9,9 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Document;
 use App\Entity\Genre;
+use App\Entity\Utilisateur;
+use App\Entity\Autorisation;
+use App\Entity\Acces;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use App\Service\FileUploader;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
@@ -22,9 +25,12 @@ class GedController extends AbstractController
     public function uploadGed(Request $request, EntityManagerInterface $manager): Response
     {
         $listeGenre = $manager->getRepository(Genre::class)->findAll();
+        $listeAutorisation = $manager->getRepository(Autorisation::class)->findAll();
         return $this->render('ged/uploadGed.html.twig', [
-            'controller_name' => "Upload d'un document",
+            'controller_name' => "Upload d'un Document",
             'listeGenre' => $listeGenre,
+            'listeAutorisation' => $listeAutorisation,
+            'listeUsers' => $manager->getRepository(Utilisateur::class)->findAll(),
         ]);
     }
 
@@ -33,37 +39,57 @@ class GedController extends AbstractController
      */
     public function insertGed(Request $request, EntityManagerInterface $manager): Response
     {
-        //Création d'un nouveau document
-        $Document = new Document();
-        //Récupération et transfert du fichier
-        $brochureFile = $request->files->get("fichier");
-        if ($brochureFile){
-            $newFilename = uniqid('', true) . "." .
-            $brochureFile->getClientOriginalExtension();
-            $brochureFile->move($this->getParameter('upload'), $newFilename);
-            //Insertion du document dans une base
-            if($request->request->get('choix') == NULL){
-                $actif = -1;
-            } else{
-                $actif=  1;
-            } 
-            $Document->setActif($actif);
-            $Document->setNom($request->request->get('nom'));
-            $Document->setTypeId($manager->getRepository(Genre::class)->findOneById($request->request->get('genre')));
-            $Document->setCreatedAt(new \Datetime);
-            $Document->setChemin($newFilename);
-    
-            $manager->persist($Document);
-            $manager->flush();
+        
+        {
+            $sess = $request->getSession();
+            //création d'un nouveau document
+            $Document = new Document();
+            //Récupération et transfert du fichier
+            //dd($request->request->get('choix'));
+            $brochureFile = $request->files->get("fichier");
+            if ($brochureFile){
+                $newFilename = uniqid('', true) . "." . $brochureFile->getClientOriginalExtension();
+                $brochureFile->move($this->getParameter('upload'), $newFilename);
+                //insertion du document dans la base.
+                if($request->request->get('choix') == "on"){
+                    $actif=1;
+                }else{
+                    $actif=2;
+                }
+                $Document->setActif($actif);
+                $Document->setNom($request->request->get('nom'));
+                $Document->setTypeId($manager->getRepository(Genre::class)->findOneById($request->request->get('genre')));
+                $Document->setCreatedAt(new \Datetime);    
+                $Document->setChemin($newFilename);    
+                
+                $manager->persist($Document);
+                $manager->flush();
+            }
+            if($request->request->get('utilisateur') != -1){
+                $user = $manager->getRepository(Utilisateur::class)->findOneById($request->request->get('utilisateur'));
+                $autorisation = $manager->getRepository(Autorisation::class)->findOneById($request->request->get('autorisation'));
+                $acces = new Acces();
+                $acces->setUtilisateurId($user);
+                $acces->setAutorisationId($autorisation);
+                $acces->setDocumentId($Document);
+                $manager->persist($acces);
+                $manager->flush();    
+            }
+            //Création d'un accès pour l'uploadeur (propriétaire)
+            $user = $manager->getRepository(Utilisateur::class)->findOneById($sess->get("idUtilisateur"));
+                $autorisation = $manager->getRepository(Autorisation::class)->findOneById(1);
+                $acces = new Acces();
+                $acces->setUtilisateurId($user);
+                $acces->setAutorisationId($autorisation);
+                $acces->setDocumentId($Document);
+                $manager->persist($acces);
+                $manager->flush();    
+            
+            return $this->redirectToRoute('listeGed');
         }
-
-        $listeGenre = $manager->getRepository(Genre::class)->findAll();
-        return $this->render('ged/uploadGed.html.twig', [
-            'controller_name' => "Upload d'un document",
-            'listeGenre' => $listeGenre,
-        ]);
+        
+        return $this->redirectToRoute('listeGed');
     }
-
     /**    
      * @Route("/listeGed", name="listeGed")
      */
@@ -83,11 +109,25 @@ class GedController extends AbstractController
      */
     public function deleteGed(Request $request, EntityManagerInterface $manager, Document $id): Response
     {
-		//Suppression de l'objet avec l'id passé en paramètre
-		$manager->remove($id);
-        $manager->flush();
-
+        $sess = $request->getSession();
+        if($sess->get("idUtilisateur")){
+        // supprimer le lien avec l'accés
+        $recupListeacces = $manager->getRepository(Acces::class)->findByDocumentId($id);
+        //dd($recupListeacces);
+        foreach($recupListeacces as $doc){
+            $manager->remove($doc);
+            $manager->flush();
+        }    
+        //suppression physique du document :
+        if(unlink("upload/".$id->getChemin())){
+        //suppression du lien dans la base de données
+            $manager->remove($id);
+            $manager->flush();
+        }
         return $this->redirectToRoute('listeGed');
+        }else{
+            return $this->redirectToRoute('authentification');    
+        }
     }
 
     /**    
